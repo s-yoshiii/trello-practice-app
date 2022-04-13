77fab3c511a6982fb0136d6c0455c2cc1398e73a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-13 23:40:14 +0900

    要素のスタイルを整える

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="dragDropArea">
       <DragDropContext>
         <Droppable droppableId="droppable">
           {(provided) => (
@@ -11,6 +11,7 @@ function App() {
               <Draggable draggableId="draggable-1" index={0}>
                 {(provided) => (
                   <div
+                    className="item"
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
