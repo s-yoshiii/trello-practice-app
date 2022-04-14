1f8787855333f78072a047979ae8bd9feb6df8a8
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-15 00:55:18 +0900

    map関数の使用

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./App.css";
 
 function App() {
-  const [items] = useState(["item0", "item1", "item2"]);
+  const [items] = useState([
+    { id: 0, text: "item0" },
+    { id: 2, text: "item1" },
+    { id: 3, text: "item2" },
+  ]);
   const onDragEnd = (result) => {
     const remove = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, remove[0]);
@@ -14,42 +18,24 @@ function App() {
         <Droppable droppableId="droppable">
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
-              <Draggable draggableId="draggable-1" index={0}>
-                {(provided) => (
-                  <div
-                    className="item"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {items[0]}
-                  </div>
-                )}
-              </Draggable>
-              <Draggable draggableId="draggable-2" index={1}>
-                {(provided) => (
-                  <div
-                    className="item"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {items[1]}
-                  </div>
-                )}
-              </Draggable>
-              <Draggable draggableId="draggable-3" index={2}>
-                {(provided) => (
-                  <div
-                    className="item"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {items[2]}
-                  </div>
-                )}
-              </Draggable>
+              {items.map((item, index) => (
+                <Draggable
+                  draggableId={item.text}
+                  index={item.id}
+                  key={item.id}
+                >
+                  {(provided) => (
+                    <div
+                      className="item"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {item.text}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
               {provided.placeholder}
             </div>
           )}
