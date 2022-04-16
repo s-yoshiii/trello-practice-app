f11e123fd57f33719e15e7eaaf4cc0a523566f20
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-16 21:39:55 +0900

    背景の設定、TaskCardコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
-import { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
+import TaskCards from "./components/task/TaskCards";
 
 function App() {
-  const [items] = useState([
-    { id: 0, text: "item0" },
-    { id: 2, text: "item1" },
-    { id: 3, text: "item2" },
-  ]);
-  const onDragEnd = (result) => {
-    const remove = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, remove[0]);
-  };
   return (
-    <>
+    <div className="app">
       <Header />
-      <div className="dragDropArea">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided) => (
-              <div ref={provided.innerRef} {...provided.droppableProps}>
-                {items.map((item, index) => (
-                  <Draggable
-                    draggableId={item.text}
-                    index={item.id}
-                    key={item.id}
-                  >
-                    {(provided) => (
-                      <div
-                        className="item"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        {item.text}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
-    </>
+      <TaskCards />
+    </div>
   );
 }
 
