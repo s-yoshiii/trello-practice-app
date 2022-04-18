f4bcf13795ebac7cac523b090d5e8d1ca7d9c0aa
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-18 09:39:54 +0900

    入力内容をstateで管理する

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-const TaskAddInput = () => {
+const TaskAddInput = ({ inputText, setInputText, taskList, setTaskList }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
   };
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+    console.log(inputText);
+  };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="add a task" className="taskAddInput" />
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
